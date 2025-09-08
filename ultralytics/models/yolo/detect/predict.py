ce1b35fb976288823891f6d19e6be54696b2add1
RIKU0929 <soccer12365478@gmail.com>
2025-09-08 19:26:29 +0900

    Update predict.py
    Signed-off-by: RIKU0929 <soccer12365478@gmail.com>

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -64,6 +64,8 @@ class DetectionPredictor(BasePredictor):
             end2end=getattr(self.model, "end2end", False),
             rotated=self.args.task == "obb",
             return_idxs=save_feats,
+            size_nms_lambda = getattr(self.args, "size_nms_lambda", 0.0),
+            size_nms_per_class=getattr(self.args, "size_nms_per_class", True),
         )
 
         if not isinstance(orig_imgs, list):  # input images are a torch.Tensor, not a list
